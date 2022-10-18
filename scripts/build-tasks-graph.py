10260bf78aec0f90a275ae8f340fbe23a6086877
Cybernetic1 <generic.intelligence@gmail.com>
2022-10-19 00:45:38 +0800

    translate into English from Chinese site

---
+++ scripts/build-tasks-graph.py
@@ -0,0 +1,54 @@
+# This script is rather unnecessary now that we have the UI to edit the
+# Project Graph....
+
+from neo4j import GraphDatabase
+import re
+
+uri = "neo4j://localhost:7687"
+driver = GraphDatabase.driver(uri, auth=("neo4j", "l0wsecurity"))
+
+"""
+def create_word(tx, chars):
+	tx.run("CREATE (a :Word {chars: $chars})",
+		chars=chars)
+
+def create_char(tx, char):
+	tx.run("CREATE (a :Char) WHERE a.char = $char",
+		char=char)
+
+def create_rel(tx, char, chars):
+	# "MERGE (b :Word {chars: $chars}) "
+	tx.run( "MERGE (a :Char {char: $char}) "
+			"MERGE (b :Word {chars: $chars}) "
+			"MERGE (a)-[:In]->(b)",
+			char=char, chars=chars)
+"""
+
+def create_node(tx, name):
+	tx.run( "CREATE (a :Node {name: $name})",
+			name=name)
+
+with driver.session() as session:
+	session.write_transaction(create_node, "Root")
+	session.write_transaction(create_node, "Business plan")
+	session.write_transaction(create_node, "Technical white paper")
+	session.write_transaction(create_node, "Web site")
+	session.write_transaction(create_node, "Git interface")
+	session.write_transaction(create_node, "Neo4j interface")
+	session.write_transaction(create_node, "Chatroom interface")
+	session.write_transaction(create_node, "DAO interface")
+
+	# Link all nodes as children of 'root'
+	session.write_transaction( lambda tx: tx.run(
+		"MATCH (a:Node), (b:Node) WHERE b.name = 'root'"
+		"CREATE (a)-[r:SubTask]->(b)"
+		) )
+
+	# Remove self-loop
+	session.write_transaction( lambda tx: tx.run(
+		"MATCH (n:Node)-[r]->(n) WHERE n.name = 'root' DELETE r"
+		) )
+
+driver.close()
+print("Successfully created Tasks graph.")
+exit(0)
