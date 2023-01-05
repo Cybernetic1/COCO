e1bd6c88ed18676c3d2d216e6f7a6b57f8a8694a
Cybernetic1 <generic.intelligence@gmail.com>
2023-01-05 18:22:30 +0800

    able to display help pages, with full height, bilingually

---
+++ js/bilingual.js
@@ -0,0 +1,8 @@
+// Make sure jQuery is working:
+// console.log("Testing", window.jQuery);
+if (window.parent.lang == "EN")
+	$('[lang="ZH"]').hide();
+else
+	$('[lang="EN"]').hide();
+// $('[lang="ZH"]').toggle();
+// $('[lang="EN"]').toggle();
